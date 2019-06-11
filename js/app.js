c1d04d3eb85fd1690deadaaccf60e955ec564025
Promila Tamanggit config --global user.email promilatam@gmail.com <YOUR EMAIL ADDRESS>
2019-06-11 02:00:36 -0700

    add-salmon

---
+++ js/app.js
@@ -0,0 +1,35 @@
+'use strict';
+CookiesLocation1={
+  Location: '1st and pike',
+  MinCust: 23,
+  MaxCust: 65,
+  AvgCookiesSales: 6.3
+}
+
+CookiesLocation2={
+  Location: 'SeaTac Airport',
+  MinCust: 3,
+  MaxCust: 24,
+  AvgCookiesSales: 1.2
+}
+
+CookiesLocation3={
+  Location: 'Seattle Center',
+  MinCust: 11,
+  MaxCust: 38,
+  AvgCookiesSales: 3.7
+}
+
+CookiesLocation4={
+  Location: 'Capitol Hill',
+  MinCust: 20,
+  MaxCust: 38,
+  AvgCookiesSales: 2.3
+}
+
+CookiesLocation5={
+  Location: 'Alki',
+  MinCust: 2,
+  MaxCust: 16,
+  AvgCookiesSales: 4.6
+}
